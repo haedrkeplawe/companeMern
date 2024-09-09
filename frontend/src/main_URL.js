c779c26ae7ae0ddaa435115695378a885cb0c9c3
haedrkeplawe <111405528+haedrkeplawe@users.noreply.github.com>
2024-09-09 04:38:25 +0300

    Update main_URL.js

--- frontend/src/main_URL.js
+++ frontend/src/main_URL.js
@@ -1,5 +1,5 @@
-// const URL = "https://companemern.onrender.com/"
-const URL = "http://localhost:4000/"
+const URL = "https://companemern.onrender.com/"
+//const URL = "http://localhost:4000/"
 
 
 export default URL
